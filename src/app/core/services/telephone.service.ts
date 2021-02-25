25e27b38dd98d7c88c410ea6b303eb597012f98b
ArturRSO <artur.rso@outlook.com>
2021-02-24 22:37:59 -0300

    Adding address and telephone service, developing user profile component

---
+++ src/app/core/services/telephone.service.ts
@@ -0,0 +1,67 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TelephoneService {
+
+  private baseApiUrl = `${environment.API_URL}/telephones`;
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  public createTelephone(telephone: any): any {
+
+    return this.http.post(`${this.baseApiUrl}/create`, telephone).pipe(
+      map(response => {
+        return response;
+      }),
+      catchError((error: any) => throwError(error))
+    );
+  }
+
+  public deleteTelephone(telephoneId: number): any {
+
+    return this.http.delete(`${this.baseApiUrl}/delete/${telephoneId}`).pipe(
+      map(response => {
+        return response;
+      }),
+      catchError((error: any) => throwError(error))
+    );
+  }
+
+  public getTelephoneById(id: number): any {
+
+    return this.http.get(`${this.baseApiUrl}/${id}`).pipe(
+      map(response => {
+        return response;
+      }),
+      catchError((error: any) => throwError(error))
+    );
+  }
+
+  public getTelephonesByUserId(userId: number): any {
+
+    return this.http.get(`${this.baseApiUrl}/user/${userId}`).pipe(
+      map(response => {
+        return response;
+      }),
+      catchError((error: any) => throwError(error))
+    );
+  }
+
+  public updateTelephone(telephone: any): any {
+
+    return this.http.put(`${this.baseApiUrl}/update`, telephone).pipe(
+      map(response => {
+        return response;
+      }),
+      catchError((error: any) => throwError(error))
+    );
+  }
+}
--- src/app/pages/registration/user-profile/user-profile.component.ts
+++ src/app/pages/registration/user-profile/user-profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AddressService } from 'src/app/core/services/address.service';
+import { AuthenticationService } from 'src/app/core/services/authentication.service';
 import { LoaderService } from 'src/app/core/services/loader.service';
 import { UserService } from 'src/app/core/services/user.service';
 import { InputMasks } from 'src/app/utils/enums/input-masks.enum';
@@ -10,10 +12,15 @@ import { InputMasks } from 'src/app/utils/enums/input-masks.enum';
 })
 export class UserProfileComponent implements OnInit {
 
+  public addresses = [];
   public documentMask = InputMasks.CPF;
+  public postalCodeMask = InputMasks.CEP;
+  public telephones = [];
   public user: any;
 
   constructor(
+    private addressService: AddressService,
+    private authService: AuthenticationService,
     private loader: LoaderService,
     private userService: UserService
   ) { }
@@ -22,13 +29,34 @@ export class UserProfileComponent implements OnInit {
     this.getProfile();
   }
 
+  public navigateToPage(route: string): void {
+    // TO DO
+    console.log(route);
+  }
+
+  public deleteAddress(addressId: number): void {
+    // TO DO
+    console.log(addressId);
+  }
+
+  public updateAddress(address: any): void {
+    // TO DO
+    console.log(address);
+  }
+
   private getProfile(): void {
     this.loader.enable();
 
-    this.userService.getProfile().subscribe(response => {
-      this.loader.disable();
+    const userId = this.authService.getAuthenticationState().userId;
 
+    this.userService.getProfile().subscribe(response => {
       this.user = response.data;
+
+      this.addressService.getAddressesByUserId(userId).subscribe(response => {
+        this.addresses = response.data;
+
+        this.loader.disable();
+      });
     });
   }
 }
